3614b8d9b1ec27fe4f0bd2f76a7ee18ed8dbbec2
Michael <mdumont567@gmail.com>
2024-10-29 23:55:40 -0300

    Websocket working

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask_smorest import Api
-from core import main_bp
 from flask_caching import Cache
+from flask_cors import CORS
+from flask_socketio import SocketIO
+import core  # Importar o módulo core
 
 app = Flask(__name__)
 cache = Cache(app)
+socketio_app = SocketIO(app, async_mode='eventlet', cors_allowed_origins='*')  # Inicialize o SocketIO aqui
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "Scraper"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config['DEBUG'] = True
 app.config["OPENAPI_URL_PREFIX"] = "/"
 
 api = Api(app)
-api.register_blueprint(main_bp)
+core.init_socketio(socketio_app)  # Passar o objeto SocketIO para o core
+
+# Registre o blueprint no app
+api.register_blueprint(core.main_bp)  
+
+# Inicie o servidor usando `socketio.run`
+if __name__ == "__main__":
+    socketio_app.run(app, host="0.0.0.0", port=5000)
--- core.py
+++ core.py
@@ -1,16 +1,70 @@
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 from flask_smorest import Blueprint
+from flask_socketio import send, SocketIO
+import requests
 
 main_bp = Blueprint("test", __name__, description="Initial test")
 
-informationsOfTarget = {}
+socketio = None
 
-@main_bp.route('/save', methods=['POST'])
-def test():
+def init_socketio(app_socketio):
+    global socketio
+    socketio = app_socketio
+    register_events() 
+
+def register_events():
+    @socketio.on('message')
+    def handle_message(msg):
+        send({'status': 'success', 'message': f'Gem: {msg}'}, broadcast=True)
+
+class TargetInformations:
+    def __init__(self, endpoint, authorization, consumers_quantity, json_payload, requests_per_consumer):
+        self.endpoint = endpoint
+        self.authorization = authorization
+        self.consumers_quantity = consumers_quantity
+        self.json_payload = json_payload
+        self.requests_per_consumer = requests_per_consumer
+
+    def to_dict(self):
+        return {
+            'endpoint': self.endpoint,
+            'authorization': self.authorization,
+            'consumers_quantity': self.consumers_quantity,
+            'json_payload': self.json_payload,
+            'requests_per_consumer': self.requests_per_consumer
+        }
+
+informationsOfTarget = TargetInformations('yourApi', 'yourToken', 1, {}, 1)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@main_bp.route('/', methods=['POST'])
+def save_target_informations():
     global informationsOfTarget
-    informationsOfTarget = request.get_json()
-    return jsonify(informationsOfTarget)
+    
+    data = request.get_json()
+    informationsOfTarget = TargetInformations(
+        data.get('endpoint'),
+        data.get('authorization'),
+        data.get('consumers_quantity'),
+        data.get('json_payload'),
+        data.get('requests_per_consumer')
+    )
 
-@main_bp.route('/test2', methods=['GET'])
-def test2():
-    return jsonify(informationsOfTarget)
+    return jsonify(informationsOfTarget.to_dict())
+
+@main_bp.route('/target-infos', methods=['GET'])
+def get_target_informations():
+    return jsonify(informationsOfTarget.to_dict())
+
+@main_bp.route('/detonate', methods=['GET'])
+def detonate():
+    madeRequests = 0
+
+    while informationsOfTarget.requests_per_consumer > madeRequests:
+        response = requests.get(informationsOfTarget.endpoint)
+        madeRequests += 1
+
+    return jsonify(response.json())

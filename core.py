03a9392e9555bb6dc7d5f97d7ba5a336fad5b8b5
Michael <mdumont567@gmail.com>
2024-10-31 00:54:08 -0300

    MVP Completo

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, render_template
 from flask_smorest import Blueprint
 from flask_socketio import send, SocketIO
 import requests
+from datetime import datetime
 
 main_bp = Blueprint("test", __name__, description="Initial test")
 
@@ -10,12 +11,7 @@ socketio = None
 def init_socketio(app_socketio):
     global socketio
     socketio = app_socketio
-    register_events() 
 
-def register_events():
-    @socketio.on('message')
-    def handle_message(msg):
-        send({'status': 'success', 'message': f'Gem: {msg}'}, broadcast=True)
 
 class TargetInformations:
     def __init__(self, endpoint, authorization, consumers_quantity, json_payload, requests_per_consumer):
@@ -44,10 +40,11 @@ class TargetInformations:
         }
 
 informationsOfTarget = TargetInformations('yourApi', 'yourToken', 1, {}, 1)
+requests_elapsed_time = []
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', metrics={})
 
 @main_bp.route('/', methods=['POST'])
 def save_target_informations():
@@ -70,36 +67,69 @@ def get_target_informations():
 
 @main_bp.route('/detonate', methods=['GET'])
 def detonate():
+    global requests_elapsed_time
+    for _ in range(int(informationsOfTarget.consumers_quantity)): 
+        socketio.start_background_task(run_detonation)
+    return jsonify('Started 5 detonation tasks')
+
+def run_detonation():
     made_requests = 0
 
-    
     while int(informationsOfTarget.requests_per_consumer) > made_requests:
         try:
             response = requests.get(informationsOfTarget.endpoint)
             elapsed_time = response.elapsed.total_seconds()
             
-            socketio.emit('message', {
+            socketio.emit('requests', {
                 'status': 'Success',
                 'content': {
-                    'requestTime': elapsed_time
+                    'executionTime': elapsed_time,
+                    'dateTime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    'requestNumber': str(made_requests)
                 }
             }, namespace='/')
-            
+
             socketio.sleep(0.1)
-            
-        except:
-            socketio.emit('message', {
+
+            send_requisitions_metrics(elapsed_time)
+
+        except Exception as e:
+            socketio.emit('requests', {
                 'status': 'Error',
                 'content': {
-                    'requestTime': elapsed_time
+                    'message': str(e),
+                    'executionTime': elapsed_time if 'elapsed_time' in locals() else 0,
+                    'dateTime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    'requestNumber': str(made_requests)
                 }
             }, namespace='/')
-            
+
             socketio.sleep(0.1)
-            
+
         made_requests += 1
-        
-        
-    return jsonify('Finished')
     
+    return jsonify('Finished detonation tasks')
+
+def send_requisitions_metrics(elapsed_time):
+    requests_elapsed_time.append(elapsed_time)
+    
+    
+    if requests_elapsed_time:
+        average_time = sum(requests_elapsed_time) / len(requests_elapsed_time)
+        max_time = max(requests_elapsed_time) 
+
+    else:
+        average_time = 0
+        max_time = 0
+
+    socketio.emit('metrics', {
+        'averageExecutionTime': average_time,
+        'totalRequests': len(requests_elapsed_time),
+        'maxExecutionTime': max_time, 
+        'dateTime': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }, namespace='/')
+
+        
+    
+
     
